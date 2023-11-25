72cd5ca8581c639695532a7d2e5aa187ccd33fa0
suitablePanther <suitablepanther@gmail.com>
2023-11-25 14:24:44 +0100

    edit the aside

--- src/components/docs/AsideTemplate.jsx
+++ src/components/docs/AsideTemplate.jsx
@@ -1,7 +1,23 @@
 import { NavLink } from "react-router-dom";
 import { docsSemestres } from "../../branchesData";
 
-export const Aside = ({ handleClick }) => {
+const Aside = () => {
+	const handleClick = (e) => {
+		const target = e.currentTarget;
+		const spans = target.nextSibling;
+		console.log(target.textContent);
+
+		if (target.textContent.trim().toLowerCase() === "mip") {
+			console.log(true);
+			spans.classList.toggle("group-first:h-fit");
+			target.firstChild.classList.toggle("group-first:rotate-0");
+			return;
+		}
+		spans.classList.toggle("h-0");
+		target.firstChild.classList.toggle("rotate-180");
+		return;
+	};
+
 	return (
 		<aside
 			className="basis-1/3 justify-start pt-32 gap-2 hidden
@@ -20,24 +36,27 @@ export const Aside = ({ handleClick }) => {
 								className="absolute right-2 top-1/2 -translate-y-1/2 text-red-300
 						 content-[url('/svgs/up.svg')] dark:content-[url('/svgs/dup.svg')] group-first:rotate-0 w-5 rotate-180"
 							></span>
-							{name}
+							{name} 
 						</span>
 						<div
 							id="semestre"
 							className="group-first:h-fit flex h-0 flex-col overflow-hidden pt-0.5 gap-1 text-center text-sm"
 						>
+							{/* semestres map  */}
 							{semestres.map((semestre) => {
 								const { id, name, path } = semestre;
 								return (
 									<NavLink
 										key={id}
 										to={path}
-										className="w-5/6 hover:text-blue font-semibold duration-100 ease-linear capitalize m-auto rounded"
+										className="w-5/6 hover:text-blue font-semibold duration-100 ease-linear
+									capitalize m-auto rounded"
 									>
 										{name}
 									</NavLink>
 								);
 							})}
+							{/* semestres map  */}
 						</div>
 					</div>
 				);
--- src/components/docs/Docs.jsx
+++ src/components/docs/Docs.jsx
@@ -4,21 +4,10 @@ import MSOne from "./semestres/mip/components/SOne";
 import Aside from "./AsideTemplate";
 const Docs = () => {
 	const { pathname } = useLocation();
-	const handleClick = (e) => {
-		const target = e.currentTarget;
-		const spans = target.nextSibling;
-		if (target.textContent.toLowerCase() === "mip") {
-			spans.classList.toggle("group-first:h-fit");
-			target.firstChild.classList.toggle("group-first:rotate-0");
-			return;
-		}
-		spans.classList.toggle("h-0");
-		target.firstChild.classList.toggle("rotate-180");
-		return;
-	};
+
 	return (
 		<section id="docs" className="flex py-0">
-			<Aside handleClick={handleClick} />
+			<Aside />
 			<main className="basis-full bg-[#eeeeee] dark:bg-sky-950/90 py-14">
 				{pathname === "/docs" ? <MSOne /> : <Outlet />}
 			</main>
